4983bf32a891464b9632012cec7d7c990cda8627
easywaldoji <easywaldoji@gmail.com>
2021-02-27 11:00:54 +0900

    Cold / Hot Publisher Test

--- src/test/java/FlowableTest.java
+++ src/test/java/FlowableTest.java
@@ -1,6 +1,7 @@
 import hu.akarnokd.rxjava3.math.MathFlowable;
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.processors.PublishProcessor;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -29,5 +30,28 @@ public class FlowableTest {
         var coldPub = Observable.just(1,2,3,4,5);
         coldPub.subscribe(x -> System.out.println("구독자 1 : " + x));
         coldPub.subscribe(x -> System.out.println("구독자 2 : " + x));
+    }
+
+    @Test
+    public void cold_publisher_test() {
+        Flowable<Integer> flowable = Flowable.just(1,3,5,7,9);
+        flowable.subscribe(data -> System.out.println("구독자 1 : " + data));
+        flowable.subscribe(data -> System.out.println("구독자 2 : " + data));
+    }
+
+    @Test
+    public void hot_publisher_test() {
+        PublishProcessor<Integer> processor = PublishProcessor.create();
+        processor.subscribe(data -> System.out.println("구독자 1 : " + data));
+        processor.onNext(1);
+        processor.onNext(3);
+
+        processor.subscribe(data -> System.out.println("구독자 2 :" + data));
+        processor.onNext(5);
+        processor.onNext(7);
+
+        processor.onComplete();
+
+
     }
 }
